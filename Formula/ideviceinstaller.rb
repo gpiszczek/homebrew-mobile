3f7faed08630ff871e3000db77df88d89652e6b7
Grzegorz Piszczek <grzegorz@piszczek.name>
2022-01-31 13:22:09 +0100

    Add custom ideviceinstaller formula

---
+++ Formula/ideviceinstaller.rb
@@ -0,0 +1,25 @@
+class Ideviceinstaller < Formula
+  desc "Tool for managing apps on iOS devices"
+  homepage "https://www.libimobiledevice.org/"
+  head "https://git.sukimashita.com/ideviceinstaller.git"
+  license "GPL-2.0"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+  depends_on "libimobiledevice"
+  depends_on "libplist"
+  depends_on "libzip"
+
+  def install
+    system "./autogen.sh" if build.head?
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/ideviceinstaller --help |grep -q ^Usage"
+  end
+end
